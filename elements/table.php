194ece99d16455e4e5cd4a5ce88412e46a8f2349
Zarko Stanisic <zarko.stanisic@live.com>
2019-07-16 15:19:17 +0200

    table add rows

--- elements/table.php
+++ elements/table.php
@@ -1,9 +1,10 @@
 <?php
   use App\Table;
-  
+
   $rows = [];
   $rows[] = ['Header Column 1', ['value' => 'Header Column 2', 'bold' => true]];
-  $rows[] = [['value' => 'Cell 2', 'bold' => true], 'Cell 1'];
+  $rows[] = [['value' => 'Row 1 Cell 1', 'bold' => true], 'Row 1 Cell 2'];
+  $rows[] = [['value' => 'Row 2 Cell 1', 'bold' => true], 'Row 2 Cell 2'];
 
   $table = new Table($rows);
  ?>
